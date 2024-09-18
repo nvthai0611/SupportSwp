c9458a1e7dce586c4d8d8f7cff50ca470ba72296
nvthai0611 <nvthai061105@gmail.com>
2024-09-18 20:49:55 +0700

    update code

---
+++ src/java/Model/Customers.java
@@ -0,0 +1,110 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author admin
+ */
+public class Customers {
+    private int customerId;
+    private String userName;
+    private String passWord;
+    private String status;
+    private String imgUrl;
+    private String phone;
+    private String email;
+    private String address;
+    private String fullName;
+
+    public Customers() {
+    }
+    
+    public Customers(int customerId, String userName, String passWord, String status, String imgUrl, String phone, String email, String address, String fullName) {
+        this.customerId = customerId;
+        this.userName = userName;
+        this.passWord = passWord;
+        this.status = status;
+        this.imgUrl = imgUrl;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.fullName = fullName;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    
+}
+
